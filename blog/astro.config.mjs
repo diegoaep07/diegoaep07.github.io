d2083ab0274b8e8649d972ee86cdb2c78bf49f7c
diegoaep07 <diegoaep07@proton.me>
2025-09-04 16:30:23 -0400

    Arreglando configuraciones de Astro

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://diegoaep07.github.io/blog',
-	base: '/blog'
+	base: '/blog',
 	integrations: [mdx(), sitemap()],
 });
