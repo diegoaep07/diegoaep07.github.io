1b5755d77e4835365f94d87b97668b16665c7ec5
diegoaep07 <diegoaep07@proton.me>
2025-09-05 09:20:19 -0400

    Personalizando el Blog

--- _blog/src/consts.ts
+++ _blog/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '1/2 de Programador';
+export const SITE_DESCRIPTION = 'El blog sobre tecnología y programación que nadie pidió y nadie necesita...';
