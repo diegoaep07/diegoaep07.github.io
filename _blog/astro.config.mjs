6f843c871d16c1affef84378a8f49508ac562cdb
Dantescur <daniel@thedaniweb.eu.org>
2025-09-10 09:03:01 -0400

    chore: added vue integration and others deps

--- _blog/astro.config.mjs
+++ _blog/astro.config.mjs
@@ -4,11 +4,13 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://diegoaep07.github.io",
   base: "/blog/",
   trailingSlash: "always",
-  integrations: [mdx(), sitemap()],
-  outDir: "../blog",
+  integrations: [mdx(), sitemap(), vue()],
+  outDir: "../blog",  
 });
