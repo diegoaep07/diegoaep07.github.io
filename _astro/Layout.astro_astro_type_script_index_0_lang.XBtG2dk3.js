8112a683d68e6c5b94407611a00932d035b45a4a
Diego <diegoaep07@proton.me>
2026-03-12 00:23:03 -0400

    Initial Commit

---
+++ _astro/Layout.astro_astro_type_script_index_0_lang.XBtG2dk3.js
@@ -0,0 +1,1 @@
+import{i as e,s as t}from"./helpers.D89mnoFx.js";e();t();document.addEventListener("astro:after-swap",()=>{t()});
---
+++ _astro/ThemeToggle.astro_astro_type_script_index_0_lang.B37mfIQu.js
@@ -0,0 +1,1 @@
+import{t as e}from"./helpers.D89mnoFx.js";const t=document.getElementById("theme-toggle");t?.addEventListener("click",()=>{e()});
