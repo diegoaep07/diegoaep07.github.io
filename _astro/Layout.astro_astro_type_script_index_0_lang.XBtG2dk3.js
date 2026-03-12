bc1def4f65bfe8a16cdd7654b8d6a1dee1ec267c
Diego <diegoaep07@proton.me>
2026-03-12 09:27:26 -0400

    Preparing the web for deploy the build in GitHub Pages

--- _astro/Layout.astro_astro_type_script_index_0_lang.XBtG2dk3.js
+++
@@ -1,1 +0,0 @@
-import{i as e,s as t}from"./helpers.D89mnoFx.js";e();t();document.addEventListener("astro:after-swap",()=>{t()});
--- _astro/ThemeToggle.astro_astro_type_script_index_0_lang.B37mfIQu.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./helpers.D89mnoFx.js";const t=document.getElementById("theme-toggle");t?.addEventListener("click",()=>{e()});
